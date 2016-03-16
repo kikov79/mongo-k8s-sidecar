2ab6debec20b5b8f09370e994001ce3aba008c04
Ubuntu <scalr@0d92600fd2c8.gq2cqdhsj5nehd5a53twdh1yde.ax.internal.cloudapp.net>
2016-03-16 03:47:47 +0000

    * Added del script

---
+++ example/del.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+NUM_REPLICAS=$(kubectl get rc -l role=mongo -o template --template='{{ len .items }}')
+NEW_REPLICA_NUM=$((NUM_REPLICAS + 1 ))
+
+echo "Current Number of MongoDB Replicas: ${NUM_REPLICAS}"
+
+echo 'Deleting Replica'
+kubectl delete svc mongo-${NUM_REPLICAS}
+kubectl delete rc mongo-${NUM_REPLICAS}
