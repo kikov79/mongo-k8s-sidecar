5f6ccadce1628f266b3c3bf8689101a0d0d165d1
Ubuntu <scalr@0d92600fd2c8.gq2cqdhsj5nehd5a53twdh1yde.ax.internal.cloudapp.net>
2016-03-17 22:38:52 +0000

    Prepare for environments

--- example/add.sh
+++ example/add.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 
-NUM_REPLICAS=$(kubectl get rc -l role=mongo -o template --template='{{ len .items }}')
+ENV=${ENV:-pre}
+NUM_REPLICAS=$(kubectl get rc -l role=mongo -l environment=${ENV} -o template --template='{{ len .items }}')
 NEW_REPLICA_NUM=$((NUM_REPLICAS + 1 ))
+
 
 echo "Current Number of MongoDB Replicas: ${NUM_REPLICAS}"
 
 echo 'Adding Replica'
-sed -e "s~<num>~${NEW_REPLICA_NUM}~g" mongo-controller-template.yaml | kubectl create -f -
+sed -e "s~<num>~${NEW_REPLICA_NUM}~g" -e "s~<env>~${ENV}~g"  mongo-controller-template.yaml | kubectl create -f -
+sed -e "s~<num>~${NEW_REPLICA_NUM}~g" -e "s~<env>~${ENV}~g"  mongo-service-template.yaml | kubectl create -f -
+azure storage share create ${ENV}-mongo-${NEW_REPLICA_NUM}
--- example/del.sh
+++ example/del.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 
-NUM_REPLICAS=$(kubectl get rc -l role=mongo -o template --template='{{ len .items }}')
+ENV=${ENV:-pre}
+NUM_REPLICAS=$(kubectl get rc -l role=mongo -l environment=${ENV} -o template --template='{{ len .items }}')
 NEW_REPLICA_NUM=$((NUM_REPLICAS + 1 ))
 
 echo "Current Number of MongoDB Replicas: ${NUM_REPLICAS}"
 
 echo 'Deleting Replica'
-kubectl delete svc mongo-${NUM_REPLICAS}
-kubectl delete rc mongo-${NUM_REPLICAS}
+kubectl delete svc ${ENV}-mongo-${NUM_REPLICAS}
+kubectl delete rc ${ENV}-mongo-${NUM_REPLICAS}
